19d87965ef7f65c8021e34e0da32049e73523386
Rafal Zw <zwierz.rafal@gmail.com>
2022-07-12 00:12:52 +0200

    Updated Sidebar content.

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -5,10 +5,15 @@ export const Sidebar = () => {
     return (
         <div className="sidebar">
             <div className="sidebarItem">
-                <span className="sidebarTitle">O MNIE</span>
-                <img
-                    src="https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?cs=srgb&dl=pexels-chloe-1043471.jpg&fm=jpg" alt=""/>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur atque dolorem, expedita facere illum in ipsum magnam, modi nam officiis perspiciatis quaerat quo quod, reiciendis sequi vel veritatis voluptate voluptatibus.</p>
+                <span className="sidebarTitle">O BLOGU</span>
+                <a title="Ramaksoud2000 via Chris Williams, Public domain, via Wikimedia Commons"
+                   href="https://commons.wikimedia.org/wiki/File:JavaScript-logo.png">
+                    <img width="512" alt="JavaScript-logo"
+                         src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/512px-JavaScript-logo.png"/></a>
+                <p>Express, Nest, React i inne biblioteki oraz frameworki JavaScript – o tym piszemy na naszym blogu
+                    poświęconym technologiom JS.
+                    Przedstawiamy zarówno podstawową wiedzę dla początkujących, jak i bardziej zaawansowane tematy z
+                    zakresu JavaScript.</p>
             </div>
             <div className="sidebarItem">
                 <span className="sidebarTitle">POPULARNE TAGI</span>
@@ -20,6 +25,14 @@ export const Sidebar = () => {
                     <li className="sidebarListItem">Angular</li>
                     <li className="sidebarListItem">Vue</li>
                 </ul>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">OBSERWUJ NAS</span>
+                <div className="sidebarSocial">
+                    <i className="sidebarIcon fa-brands fa-facebook-square"></i>
+                    <i className="sidebarIcon fa-brands fa-twitter-square"></i>
+                    <i className="sidebarIcon fa-brands fa-instagram-square"></i>
+                </div>
             </div>
         </div>
     )
